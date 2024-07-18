a1625822261172c627a811d0147dc437a3df1d18
Abdulrahman <rahman3000@mail.ru>
2024-07-19 01:53:07 +0300

    feat: add get folders and styles

--- src/app/lib/MainPageLayouts/Menu.tsx
+++ src/app/lib/MainPageLayouts/Menu.tsx
@@ -1,56 +1,62 @@
-"use client";
-import { useStoreAuth } from "../../../store/Auth";
-import Image from "next/image";
-import OpenFolder from "/public/Open_Folder.svg";
-import CloseFolder from "/public/Close_Folder.svg";
-import PlusFolder from "/public/Plus_Folder.svg";
-import Plus from "/public/Plus.svg";
-import { useState } from "react";
-
+'use client';
+import Image from 'next/image';
+import OpenFolder from '/public/Open_Folder.svg';
+import CloseFolder from '/public/Close_Folder.svg';
+import PlusFolder from '/public/Plus_Folder.svg';
+import Plus from '/public/Plus.svg';
+import { useState, useEffect } from 'react';
+import { useStoreFolder } from '@/store/Folders';
 const Menu = () => {
-  const login = useStoreAuth((state) => state.login);
+  const { createFolders, folders, getFolders, getByIdFolders, deleteByIdFolders, patchByIdFolders } = useStoreFolder(
+    (state) => state
+  );
+  useEffect(() => {
+    getFolders();
+  }, []);
 
-  const getMe = useStoreAuth((state) => state.getMe);
-
-  const token = useStoreAuth((state) => state.token);
-  console.log(token + " Menu");
-
-  const [folderOpen, setFoldlerOpen] = useState<boolean>(false);
+  const [openedFolderIds, setOpenedFolderIds] = useState(new Set());
+  const toggleFolder = (id) => {
+    console.log(id);
+    
+    setOpenedFolderIds(prev => {
+      const newOpened = new Set(prev);
+      if (newOpened.has(id)) {
+        newOpened.delete(id);
+      } else {
+        newOpened.add(id);
+      }
+      return newOpened;
+    });
+  };
+  
+  
   return (
-    <div className="border w-[15%] border-red-800 border-1">
+    <div className='border w-[15%] border-red-800 border-1'>
       <div>
-        <div className=" my-5 mx-[15px] bg-[#242424] flex justify-center rounded-[5px] h-[45px]">
+        <div className=' my-5 mx-[15px] bg-[#242424] flex justify-center rounded-[5px] h-[45px]'>
           <button>New Note</button>
-          <Image className="mx-2 w-[15px]" src={Plus} alt="Add folder button" />
+          <Image className='mx-2 w-[15px]' src={Plus} alt='Add folder button' />
         </div>
       </div>
+
       <div>
-        <div className="flex justify-between px-[20px]">
+        <div className='flex justify-between px-[20px]'>
           <h2>Folders</h2>
-          <Image
-            className="cursor-pointer w-[20px]"
-            src={PlusFolder}
-            alt="Add folder button"
-          />
+          <Image className='cursor-pointer w-[20px]' src={PlusFolder} alt='Add folder button' />
         </div>
-        <ul>
-          <div
-            onClick={() => setFoldlerOpen(!folderOpen)}
-            className="flex bg-[red] px-[20px]"
-          >
-            <Image
-              className="w-[20px]"
-              src={folderOpen ? OpenFolder : CloseFolder}
-              alt="folder"
-            />
-            <li className="mx-2">1</li>
-          </div>
-          <div className="flex bg-[blue] px-[20px]">
-            <Image className="w-[20px]" src={CloseFolder} alt="folder" />
-            <li className="mx-2">2</li>
-          </div>
-        </ul>
       </div>
+      <ul>
+        {folders.map((item) => (
+        <>
+       { console.log(item)}
+        
+          <div key={item._id} onClick={() => toggleFolder(item._id)} className={'flex px-[20px] ' + (openedFolderIds.has(item._id) ? "bg-blue-600 rounded-sm" : null)}>
+            <Image src={openedFolderIds.has(item._id) ? OpenFolder : CloseFolder} alt='Folder icon' width={20} height={20} />
+            <li className='mx-2'>{item.name}</li>
+          </div>
+        </>
+        ))}
+      </ul>
     </div>
   );
 };
